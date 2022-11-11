a9cb551ba3e2076dc98e6f366f9228ea1c3d70c3
bramos289 <bramos289@misena.edu.co>
2022-11-10 19:10:37 -0500

    Update settings.py

--- comGStore/settings.py
+++ comGStore/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gu%cnzl$u%&bj3=08@gry)=^+s!4j8=ujc352ou0(&_c#$hq%r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
